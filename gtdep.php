4489a72f48ea7debd01fdb65470e28c738e1f225
viniciusfelix-lgtn <70050734+viniciusfelix-lgtn@users.noreply.github.com>
2022-03-23 11:12:57 -0300

    Update gtdep.php

--- gtdep.php
+++ gtdep.php
@@ -1,1 +1,4 @@
-
+https://drive.google.com/uc?export=download&id=1gUzNoU3mcv2pxpqPGiOQZ9AkvD0Efqpp
+https://drive.google.com/uc?export=download&id=1P-d42mnO7lf__vuHY6EKbpFMrubV6fH0
+https://drive.google.com/uc?export=download&id=1qnk_yJBQ4a2jvdte8U8deJIHJZjJ0a9h
+https://drive.google.com/uc?export=download&id=1UyvQ-xKXT0ZMW_l_iN1UZz1Of_Us1yXr
