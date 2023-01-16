56ae4317791f69bcab384c763356ced75348adc7
viniciusfelix-lgtn <70050734+viniciusfelix-lgtn@users.noreply.github.com>
2023-01-16 08:39:42 -0300

    Update dldgt.php

--- dldgt.php
+++ dldgt.php
@@ -1,1 +1,1 @@
-https://cdn.discordapp.com/attachments/1062854694491390132/1063774062515605534/EasyGirl.SA.dll
+https://cdn.discordapp.com/attachments/1062854694491390132/1064509188690169946/EasyGirl.SA.dll
